3692c77976f2edc94c2e67fdcc07357ae83b4756
fractal <circle@ubuntu.(none)>
2012-10-10 02:27:17 +0200

    dynamic layout index pages

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,8 +4,15 @@ attr_accessible :position_name, :job_title, :valid_from, :job_grade, :reports_to
 		:attitude1, :attitude2, :attitude3, :attitude4, :attitude5, :picture, :conceptual, :implementation,
 		:support, :gender_string, :personal_development, :nationality_string, :residential_status_string, :start_date,
 		:office, :unique_position, :fte, :permanent, :department
-has_many :competencies 
+has_many :competencies
 accepts_nested_attributes_for :competencies, allow_destroy: true
 validates_presence_of :job_title, :department
+
+def status
+  if name == ""
+    "Vacant"
+  else "Filled"
+  end
 end
 
+end
